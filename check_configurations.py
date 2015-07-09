a7ebbbf892b592e3f53ee6fb973c679bf52be534
Markus Scherer <markus.f.scherer@gmail.com>
2015-07-09 16:50:07 +0200

    Changes output from dlv to dimacs.

--- check_configurations.py
+++ check_configurations.py
@@ -3,10 +3,31 @@ from DomainRestriction import is_single_peaked
 from itertools import permutations, chain, product
 from sys import argv
 
-def print_conflicts_dlv(conflicts):
-    for n,votes in enumerate(conflicts):
-        for v in votes:
-           print("c({0},{1}).".format(n,v)) 
+def print_conflicts_wcnf(conflicts, election):
+    # the sum of violated soft clauses is smaller than the number of votes
+    top = election[3] + 1
+    weights = election[2]
+    number_of_variables = len(weights)
+    number_of_clauses = len(conflicts) + number_of_variables
+
+    parameter_line =  "p wcnf "
+    parameter_line += str(number_of_variables) + " "
+    parameter_line += str(number_of_clauses) + " " + str(top)
+
+    print(parameter_line)
+
+    for i in conflicts:
+        hard_clause = str(top) + " "
+        for j in i:
+            hard_clause += str(j) + " "
+        hard_clause += "0"
+        print(hard_clause)
+
+    for i,w in enumerate(weights,1):
+        soft_clause = str(w) + " -" + str(i) + " 0"
+        print(soft_clause)
+
+
 
 filename = argv[1]
 election = read_election_file(open(filename))
@@ -24,7 +45,7 @@ worst_diverse = [[(1,3), (2,3)], [(1,2), (3,2)], [(2,1), (3,1)]]
 configurations = [alpha,worst_diverse]
 
 # Function used to print the conflicts as encoding
-print_conflicts = print_conflicts_dlv
+print_conflicts = print_conflicts_wcnf
 
 hits = [list() for _ in configurations]
 conflicts = set()
@@ -46,6 +67,7 @@ for icf, configuration in enumerate(configurations):
                         match = False
                         break
                 if match:
+                    # TODO: abstract a function which either adds votes or candidates
                     matches[ic].append((iv, vote))
         matched_votes = [[y[0] for y in u] for u in matches]
         if all(matched_votes):
@@ -53,4 +75,4 @@ for icf, configuration in enumerate(configurations):
             for combination in combinations:
                 conflicts.add(combination)
 
-print_conflicts(conflicts)
+print_conflicts(conflicts, election)
